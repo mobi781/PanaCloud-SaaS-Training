106c7a445d3f69f8ee5290903a558300c1ec2b0e
Mubashar Toseef <mrmobi62@gmail.com>
2021-05-21 23:23:05 +0500

    start expensetracker

---
+++ first_react_app/src/Components/Balance.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Balance = () => {
+    return (
+        <div>
+            <span><h3>Balance</h3></span>
+            <span><h1>$ 1000</h1></span>
+
+        </div>
+    )
+}
+
+export default Balance
---
+++ first_react_app/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Expense Tracker By Mubashar</h1>
+        </div>
+    )
+}
+
+export default Header
