44eedba26ba16f926a2dfb0f0a6c7bfd049995a7
Mubashar Toseef <mrmobi62@gmail.com>
2021-05-22 08:13:21 +0500

    little improvement

--- first_react_app/src/Components/AddNew.js
+++ first_react_app/src/Components/AddNew.js
@@ -3,30 +3,33 @@ import React from 'react'
 const AddNew = () => {
     return (
         <div>
-            <div className="form-control">
-                <label htmlFor="Description">
-                    Description
+            <h3>Add Transactions</h3>
+            <div className="form">
+                <div className="form-control">
+                    <label htmlFor="Description">
+                        Description
 
 
                 </label>
-                <input
-                    type="text"
-                    placeholder="Plaese Enter the transation detail"
-                    id="Description"
-                />
-            </div>
-            <div className="form-control">
-                <label htmlFor="Description Amount">
-                    Amount
+                    <input
+                        type="text"
+                        placeholder="Plaese Enter the transation detail"
+                        id="Description"
+                    />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="Description Amount">
+                        Amount
                     </label>
-                <input
-                    type="number"
-                    placeholder="Plaese Enter the amount"
-                    id="Description Amount"
-                />
+                    <input
+                        type="number"
+                        placeholder="Plaese Enter the amount"
+                        id="Description Amount"
+                    />
 
+                </div>
+                <button className="btn">Add Transaction</button>
             </div>
-            <button className="btn">Add Transaction</button>
 
         </div>
     )
